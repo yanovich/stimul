5c5beb2a31c95a3351ec716c57b23c969bf1f2dc
Tolstobrov Alexey <dkn2002@mail.ru>
2018-09-18 16:56:03 +0300

    fixes

--- src/components/LeftNav.js
+++ src/components/LeftNav.js
@@ -13,78 +13,79 @@ class LeftNav extends Component {
   }
 
   render() {
+
     return (
-        <Fragment>
+      <Fragment>
         <nav className="left-nav">
-        <div className="nav-button" name="profile">
-        <Link
-                className="link dim black b f6 f5-ns dib mr3"
-                to="/login"
-                title="Root"
-              >
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-            <path d="M9 11.75c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zm6 0c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8 0-.29.02-.58.05-.86 2.36-1.05 4.23-2.98 5.21-5.37C11.07 8.33 14.05 10 17.42 10c.78 0 1.53-.09 2.25-.26.21.71.33 1.47.33 2.26 0 4.41-3.59 8-8 8z"/>
-            <path fill="none" d="M0 0h24v24H0z"/>
-        </svg>
-        </Link>
-        </div>
-        <div className="nav-button" name="chat" onClick={()=>{this.props.lstate('chat')}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-            <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-        </svg><div className="indic g">15</div>
-        </div>
-        <div className="nav-button" name="kan-root">
+          <div className="nav-button" name="profile">
             <Link
-                className="link dim black b f6 f5-ns dib mr3"
-                to="/"
-                title="Root"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-                <path d="M0 0h24v24H0z" fill="none"/>
-                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM8 17.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5zM9.5 8c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5S9.5 9.38 9.5 8zm6.5 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/>
-                </svg>
+              className="link dim black b f6 f5-ns dib mr3"
+              to="/login"
+              title="Root"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+                <path d="M9 11.75c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zm6 0c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8 0-.29.02-.58.05-.86 2.36-1.05 4.23-2.98 5.21-5.37C11.07 8.33 14.05 10 17.42 10c.78 0 1.53-.09 2.25-.26.21.71.33 1.47.33 2.26 0 4.41-3.59 8-8 8z" />
+                <path fill="none" d="M0 0h24v24H0z" />
+              </svg>
+            </Link>
+          </div>
+          <div className="nav-button" name="chat" onClick={() => { this.props.lstate('chat') }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+              <path d="M20 2H4c-1.1 0-1.99.9-1.99 2L2 22l4-4h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zM6 9h12v2H6V9zm8 5H6v-2h8v2zm4-6H6V6h12v2z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg><div className="indic g">15</div>
+          </div>
+          <div className="nav-button" name="kan-root">
+            <Link
+              className="link dim black b f6 f5-ns dib mr3"
+              to="/"
+              title="Root"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none" />
+                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zM8 17.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5zM9.5 8c0-1.38 1.12-2.5 2.5-2.5s2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5S9.5 9.38 9.5 8zm6.5 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
+              </svg>
             </Link>
 
-        </div>
+          </div>
 
-        <div className="nav-button" name="map">
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-            <path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-        </svg>
-        <div className="indic">3</div>
-        </div>
-        <div className="nav-button" name="search">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/>
-            <path d="M0 0h24v24H0z" fill="none"/>
-        </svg>
-        </div>
+          <div className="nav-button" name="map">
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+              <path d="M12 10.9c-.61 0-1.1.49-1.1 1.1s.49 1.1 1.1 1.1c.61 0 1.1-.49 1.1-1.1s-.49-1.1-1.1-1.1zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm2.19 12.19L6 18l3.81-8.19L18 6l-3.81 8.19z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+            <div className="indic">3</div>
+          </div>
+          <div className="nav-button" name="search">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
+              <path d="M0 0h24v24H0z" fill="none" />
+            </svg>
+          </div>
 
-        <div className="nav-button" name="fire" onClick={()=>{this.props.lstate('fire')}}>
+          <div className="nav-button" name="fire" onClick={() => { this.props.lstate('fire') }}>
 
             <svg xmlns="http://www.w3.org/2000/svg" className="fill-rd" width="24" height="24" viewBox="0 0 24 24">
-                <path d="M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.77-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z"/>
-                <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M13.5.67s.74 2.65.74 4.8c0 2.06-1.35 3.73-3.41 3.73-2.07 0-3.63-1.67-3.63-3.73l.03-.36C5.21 7.51 4 10.62 4 14c0 4.42 3.58 8 8 8s8-3.58 8-8C20 8.61 17.41 3.8 13.5.67zM11.71 19c-1.78 0-3.22-1.4-3.22-3.14 0-1.62 1.05-2.76 2.81-3.12 1.77-.36 3.6-1.21 4.62-2.58.39 1.29.59 2.65.59 4.04 0 2.65-2.15 4.8-4.8 4.8z" />
+              <path d="M0 0h24v24H0z" fill="none" />
             </svg>
-        </div>
+          </div>
 
-        <div className="nav-button" name="favor" onClick={()=>{this.props.lstate('favor')}}>
+          <div className="nav-button" name="favor" onClick={() => { this.props.lstate('favor') }}>
             <svg xmlns="http://www.w3.org/2000/svg" className="fill-yl" width="24" height="24" viewBox="0 0 24 24">
-                <path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z"/>
-                <path d="M0 0h24v24H0z" fill="none"/>
+              <path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z" />
+              <path d="M0 0h24v24H0z" fill="none" />
             </svg>
-        </div>
+          </div>
 
-        <div className="nav-button" name="help" onClick={()=>{this.props.lstate('help')}}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
-            <path d="M0 0h24v24H0z" fill="none"/>
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/>
-        </svg>
-        </div>
+          <div className="nav-button" name="help" onClick={() => { this.props.lstate('help') }}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24">
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z" />
+            </svg>
+          </div>
         </nav>
-        </Fragment>
+      </Fragment>
     )
   }
 }
--- src/components/Proj.js
+++ src/components/Proj.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Query } from 'react-apollo';
-import  { gql } from 'apollo-boost';
-import AddGroup from './AddGroup';
-import {quf} from '../constants';
+import { gql } from 'apollo-boost';
+// import AddGroup from './AddGroup';
+import { quf } from '../constants';
 
 
 
 const delPRG = (thisid) => {
-  return(
-      `mutation{
+  return (
+    `mutation{
           deleteProjectGroup(id: ${thisid})
       }`
-      )
+  )
 };
 
 
@@ -20,20 +20,20 @@ export default class Proj extends Component {
   constructor(props) {
     super(props)
     this.state = {
-        input: [],
+      input: [],
     };
     this.delete = this.delete.bind(this);
-}
+  }
 
-delete(){
-  let id = this.props.proj.id;
-  let crprg = delPRG(id);
-  quf(crprg);
-  this.props.refresh()
-  this.setState({
-    input: [],
-  });
-}
+  delete() {
+    let id = this.props.proj.id;
+    let crprg = delPRG(id);
+    quf(crprg);
+    this.props.refresh()
+    this.setState({
+      input: [],
+    });
+  }
 
 
 
@@ -41,22 +41,24 @@ delete(){
     let title = "";
     let __typename = "projectgroup";
     let projlen = 0;
-    if(this.props.proj.projects && this.props.proj.projects.length > 0 ){
+
+    if (this.props.proj.projects && this.props.proj.projects.length > 0) {
       projlen = this.props.proj.projects.length;
     }
-      if(this.props.proj.__typename === "ProjectGroup" && this.props.end == 1){__typename = 'projects'}
-      else if(this.props.proj.__typename === "ProjectGroup"){__typename = 'projectgroup'}
-      else if(this.props.proj.__typename === "Project"){__typename = 'project'}
-      else{}
-      return (
-        <Query query={GCOL_BID} variables={{pid: this.props.proj.id}}>
+    if (this.props.proj.__typename === "ProjectGroup" && this.props.end == 1) { __typename = 'projects' }
+    else if (this.props.proj.__typename === "ProjectGroup") { __typename = 'projectgroup' }
+    else if (this.props.proj.__typename === "Project") { __typename = 'project' }
+    else { return false }
+
+    return (
+      <Query query={GCOL_BID} variables={{ pid: this.props.proj.id }}>
         {({ loading, error, data, refetch }) => {
           if (loading) return "Loading...";
-          if (error){
+          if (error) {
             console.log(error.message);
             //return(<div className="errorMessage">`Error! in C`</div>);
           }
-          
+
           // let hmcols = data.columns.length;
           // let hmtasks = (data)=>{
           //   let col = 0;
@@ -65,35 +67,35 @@ delete(){
           //   });
           //   return col;
           // }
-          
-          return(
+
+          return (
             <div className="card">
               <Link className="no-underline ma1" to={`/${__typename}/${this.props.proj.id}`} type={this.props.proj.__typename} end={this.props.end}>
-              <h2 className="card-name">{this.props.proj.title}{this.props.proj.name}</h2>
-              <p className="card-id small">id {this.props.proj.id}</p>
+                <h2 className="card-name">{this.props.proj.title}{this.props.proj.name}</h2>
+                <p className="card-id small">id {this.props.proj.id}</p>
               </Link>
               {/* {console.log(projlen)} */}
               {projlen ? (<div className="projs">Проектов <span>{projlen}</span></div>) : ""}
-              
+
               <div className="semafor">
 
-              {/* <span className="y">2</span>
+                {/* <span className="y">2</span>
               <span className="g">5</span>
               <span className="r">1</span> */}
               </div>
-              
+
               <svg className="add-favorite hover" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
-                  <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
-                  <path fill="none" d="M0 0h18v18H0z"/>
+                <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z" />
+                <path fill="none" d="M0 0h18v18H0z" />
               </svg>
-              <div className="delete open hover" onClick={()=>{this.delete()}}>удалить</div>
+              <div className="delete open hover" onClick={() => { this.delete() }}>удалить</div>
             </div>
 
           )
 
         }}
-        </Query>
-      )
+      </Query>
+    )
   }
 }
 export const GCOL_BID = gql`
