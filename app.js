ad64f93eb133cfe633e02c228ee1be1d1ce927fc
Sergei Ianovich <s@asutp.io>
2019-06-04 14:06:50 +0300

    srv: fail in 'production' w/o the bundle

--- app.js
+++ app.js
@@ -8,11 +8,14 @@
  * Stimul
  */
 
+const fs = require('fs')
 const path = require('path')
 const webpack = require('webpack')
 const webpack_dev_middleware = require('webpack-dev-middleware')
 const webpack_hot_middleware = require('webpack-hot-middleware')
 const express = require('express')
+
+const paths = require('./config/paths')
 
 const app = express()
 
@@ -34,6 +37,10 @@ if (global.is_development) {
   app.use(webpack_dev_middleware(compiler, { color: true }))
   app.use(webpack_hot_middleware(compiler))
 } else if (global.is_production) {
+  if (!fs.existsSync(paths.appBuild + '/index.html')) {
+    console.error('stimul: web app not present in production, exiting')
+    process.exit(1)
+  }
   app.use(express.static(path.join(__dirname, 'build/')))
 }
 
