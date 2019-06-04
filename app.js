5a735c1d77deeadb4874878e38eaffe5c762b219
Sergei Ianovich <s@asutp.io>
2019-06-04 14:03:41 +0300

    srv: run in 'production' be default

--- app.js
+++ app.js
@@ -17,14 +17,24 @@ const express = require('express')
 const app = express()
 
 if (process.env.NODE_ENV === 'development') {
+  global.is_development = true
+} else if (process.env.NODE_ENV === 'test') {
+  global.is_test = true
+} else {
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn("stimul: running in 'production' mode by default")
+    process.env.NODE_ENV = 'production'
+  }
+  global.is_production = true
+}
+
+if (global.is_development) {
   const config = require('./config/webpack.config')('development')
   const compiler = webpack(config)
   app.use(webpack_dev_middleware(compiler, { color: true }))
   app.use(webpack_hot_middleware(compiler))
-} else if (process.env.NODE_ENV === 'production') {
+} else if (global.is_production) {
   app.use(express.static(path.join(__dirname, 'build/')))
-} else {
-  process.exit(1)
 }
 
 app.listen(3000, () => console.info('stimul: listening on port 3000'))
