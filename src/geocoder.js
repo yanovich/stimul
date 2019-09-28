205521aafc6a3af473ae058a3adf9673c7be807b
Yury Savin <yury@savin.dev>
2019-09-28 20:30:07 +0300

    Add yandex.geocoder

--- src/geocoder.js
+++ src/geocoder.js
@@ -5,3 +5,9 @@ export default function geocoder(query) {
     .then(response => response.json())
     .then(data => data.features);
 }
+
+export function yandex(query) {
+  return fetch(
+    `https://geocode-maps.yandex.ru/1.x/?apikey=704c238a-9f36-422d-8e2a-cff130abb7f4&format=json&geocode=${query}`
+  ).then(response => response.json());
+}
